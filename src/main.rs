a98151f96d92f544062868b36d20d17a9e731c43
Tuyen <michael.ross.mckay@gmail.com>
2021-12-23 21:32:16 -0500

    add environment support, make dotenv optional, add affirmation action

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
+#[cfg(feature = "dev")] 
 extern crate dotenv;
-use dotenv::dotenv;
+
+#[cfg(feature = "dev")]
+use {
+  dotenv::dotenv
+};
+
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
 use serenity::model::channel::Message;
 use serenity::framework::standard::{
-    StandardFramework,
-    CommandResult,
-    macros::{
-        command,
-        group
-    }
+  StandardFramework,
+  CommandResult,
+  macros::{
+    command,
+    group
+  }
 };
 
 use std::env;
 
 #[group]
-#[commands(ping)]
+#[commands(ping,ami)]
 struct General;
 
 struct Handler;
@@ -25,29 +31,41 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+  #[cfg(feature = "dev")]
+  dotenv().ok();
 
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
-        .group(&GENERAL_GROUP);
+  let framework = StandardFramework::new()
+    .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
+    .group(&GENERAL_GROUP);
 
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let mut client = Client::builder(token)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
+  // Login with a bot token from the environment
+  let token = env::var("DISCORD_TOKEN").expect("token");
+  let mut client = Client::builder(token)
+    .event_handler(Handler)
+    .framework(framework)
+    .await
+    .expect("Error creating client");
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
-    }
+  // start listening for events by starting a single shard
+  if let Err(why) = client.start().await {
+    println!("An error occurred while running the client: {:?}", why);
+  }
 }
+
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
+  msg.reply(ctx, "Pong!").await?;
 
-    Ok(())
+  Ok(())
+}
+
+
+#[command]
+async fn ami(ctx: &Context, msg: &Message) -> CommandResult {
+  let words: Vec<&str> = msg.content.split(" ").collect();
+  let strip_command_and_rephrase = words[1..].join(" ").replace("?", ".");
+  msg.reply(ctx, format!("`Yes, {}, you are {}.`", msg.author.name, strip_command_and_rephrase)).await?;
+
+  Ok(())
 }
