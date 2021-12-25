1aa8457d94a62e5c3faa2c4a1f4b9a008f66751e
Tuyen <michael.ross.mckay@gmail.com>
2021-12-24 20:25:43 -0500

    overflows during multiplication

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ async fn main() {
 #[command]
 async fn roll(ctx: &Context, msg: &Message) -> CommandResult {
   match msg.content.roll() {
-    Ok(n) => msg.reply(ctx, format!("hi! {}", n.total)).await?,
+    Ok(n) => msg.reply(ctx, format!("```{}```", n.to_string())).await?,
     Err(e) => msg.reply(ctx, format!("{}", e)).await?
   };
 
@@ -71,6 +71,7 @@ async fn ami(ctx: &Context, msg: &Message) -> CommandResult {
     , msg.content
       .strip_command()
       .replace("?", "")
+      .replace("my", "your")
   ).to_string();
 
   msg.reply(ctx, wisdom).await?;
@@ -163,94 +164,101 @@ impl Rollable for str {
       .filter(|c| c.is_numeric() || c == &' ' || c.to_lowercase().to_string() == "d")
       .collect();
 
-    println!("first pass: |{}|", clean_string);
-
     while clean_string.contains("  ") {
       clean_string = clean_string.replace("  ", " ")
     }
 
-    println!("second pass: |{}|", clean_string);
-
     let mut is_seeking_multiplier = true;
     let mut current_multiplier = String::new();
     let mut current_faces = String::new();
-    let chars = clean_string.chars().peekable();
+    let mut chars = clean_string.chars().peekable();
 
     println!("Looking for multipliers:");
-    while let c = chars.next() {
-      println!("  current letter: {}", c.unwrap());
-
-      if is_seeking_multiplier {
-        match c.unwrap().is_numeric() || c.unwrap() == ' ' {
-          true => if c.unwrap() != ' ' { 
-            current_multiplier.push(c.unwrap()); 
-            println!("found multiplier value");
-          },
-          false => {
-            is_seeking_multiplier = false;
-            println!("Looking for faces.");
-          }
-        };
-
-      } else {
-
-        match c.unwrap().is_numeric() && chars.peek().is_some() {
-          true => current_faces.push(c.unwrap()),
-          false => {
-            match (current_multiplier.parse::<u16>(), current_faces.parse::<u16>()) {
-              (Ok(multiplier), Ok(faces)) => {
-                println!("Nice! adding dice roll: {} x d{}", multiplier, faces);
-                let rolls = roll_result.rolls
-                  .entry(format!("d{}", faces))
-                  .or_insert(RollSet { 
-                    faces: faces
-                    , multiplier: 0
-                    , total: 0
-                    , results: Vec::new()
-                });
-
-                rolls.multiplier += multiplier;
-                let result = (multiplier * faces) as u32;
-                roll_result.total += result as u128;
-                rolls.total += result as u128;
-                rolls.results.push(result);
-                current_faces = String::new();
-                current_multiplier = String::new();
-                is_seeking_multiplier = true;
-                println!("Looking for multipliers.");
+    'charloop: loop {
+      match chars.next() {
+        Some(c) => {
+          println!("  current letter: {}", c);
+          if is_seeking_multiplier {
+            match c.is_numeric() || c == ' ' {
+              true => if c != ' ' { 
+                current_multiplier.push(c); 
+                println!("found multiplier value");
               },
-              (Err(e), Err(e2)) => return Err(format!("\nNeither '{}' nor '{}' make a parsable dice roll:\n {} \n\n  ERROR: {}", current_multiplier, current_faces, e, e2)),
-              (Ok(_n), Err(e)) => return Err(format!("\nThe multiplier '{}' looks right to me, but d'{}' cannot be parsed as a dice string:\n\n  ERROR: {}", current_multiplier, current_faces, e)),
-              (Err(e), Ok(_n)) => return Err(format!("\n'{}' cannot be parsed as a multiplier, but  d'{}' looks like a nice dice string:\n\n  ERROR: {}", current_multiplier, current_faces, e))
+              false => {
+                if c == 'd' && chars.peek().unwrap() == &' ' {
+                  chars.next();
+                }
+                is_seeking_multiplier = false;
+                println!("Looking for faces.");
+              }
             };
+          } else {
+            match c.is_numeric() && chars.peek().is_some() {
+              true => current_faces.push(c),
+              false => {
+                if chars.peek().is_none() {
+                  if c.is_numeric() { current_faces.push(c) }
+                } else {
+                  println!("|{}|", c.to_string());
+                  if c == 'd' && chars.peek().unwrap() == &' ' {
+                    continue 'charloop;
+                  }
+                }
+                match (current_multiplier.parse::<u16>(), current_faces.parse::<u16>()) {
+                  (Ok(multiplier), Ok(faces)) => {
+                    println!("Nice! adding dice roll: {} x d{}", multiplier, faces);
+                    let rolls = roll_result.rolls
+                      .entry(format!("d{}", faces))
+                      .or_insert(RollSet { 
+                        faces: faces
+                        , multiplier: 0
+                        , total: 0
+                        , results: Vec::new()
+                    });
+
+                    rolls.multiplier += multiplier;
+                    let result = (multiplier * faces) as u32;
+                    roll_result.total += result as u128;
+                    rolls.total += result as u128;
+                    rolls.results.push(result);
+
+                    if c == 'd' {
+                      current_multiplier = "1".to_string();
+                    } else {
+                      current_multiplier = String::new();
+                      is_seeking_multiplier = true;
+                    }
+                    current_faces = String::new();
+                    println!("Looking for multipliers.");
+                  },
+                  (Err(e), Err(e2)) => return Err(format!(
+                    "```\nNeither '{}' nor '{}' make a parsable dice roll:\n {} \n\n  ERROR: {} \n VALID DICE: {}```"
+                    , current_multiplier
+                    , current_faces
+                    , e
+                    , e2
+                    , roll_result.to_string()
+                  )),
+                  (Ok(_n), Err(e)) => return Err(format!(
+                    "```\nThe multiplier '{}' looks right to me, but d'{}' cannot be parsed as a dice string:\n\n  ERROR: {}, \n VALID DICE: {}```"
+                    , current_multiplier
+                    , current_faces
+                    , e
+                    , roll_result.to_string()
+                  )),
+                  (Err(e), Ok(_n)) => return Err(format!(
+                    "\n```'{}' cannot be parsed as a multiplier, but  d'{}' looks like a nice dice string:\n\n  ERROR: {}```"
+                    , current_multiplier
+                    , current_faces,
+                     e
+                  ))
+                };
+              }
+            }
           }
         }
+        _ => break,
       }
-    }
-    match (current_multiplier.parse::<u16>(), current_faces.parse::<u16>()) {
-      (Ok(multiplier), Ok(faces)) => {
-        println!("Nice! adding dice roll: {} x d{}", multiplier, faces);
-        let rolls = roll_result.rolls
-          .entry(format!("d{}", faces))
-          .or_insert(RollSet { 
-            faces: 0
-            , multiplier: 0
-            , total: 0
-            , results: Vec::new()
-        });
-
-        rolls.multiplier += multiplier;
-        rolls.faces = faces;
-        let result = (multiplier * faces) as u32;
-        roll_result.total += result as u128;
-        rolls.total += result as u128;
-        rolls.results.push(result);
-        println!("Looking for multipliers.");
-
-      },
-      (Err(e), Err(e2)) => return Err(format!("\nNeither '{}' nor '{}' make a parsable dice roll:\n {} \n\n  ERROR: {}", current_multiplier, current_faces, e, e2)),
-      (Ok(_n), Err(e)) => return Err(format!("\nThe multiplier '{}' looks right to me, but d'{}' cannot be parsed as a dice string:\n\n  ERROR: {}", current_multiplier, current_faces, e)),
-      (Err(e), Ok(_n)) => return Err(format!("\n'{}' cannot be parsed as a multiplier, but  d'{}' looks like a nice dice string:\n\n  ERROR: {}", current_multiplier, current_faces, e))
     }
     Ok(roll_result)
   }
@@ -270,7 +278,15 @@ fn test_roll() {
     Ok(n) => println!("{}", n.to_string()),
     Err(e) => println!("{}", e) 
   };
+  match "!roll 1d2ferfag23498d892838  j j j j34d 3 j34j d0fefa2d40".roll() {
+    Ok(n) => println!("{}", n.to_string()),
+    Err(e) => println!("{}", e) 
+  };
   match "!roll 1d2ferfag0fefa2d40".roll() {
+    Ok(n) => println!("{}", n.to_string()),
+    Err(e) => println!("{}", e) 
+  };
+  match "!roll 1d 480598309 fwaefj efjij 4d5t969 fejfeijijfj4d438".roll() {
     Ok(n) => println!("{}", n.to_string()),
     Err(e) => println!("{}", e) 
   };
